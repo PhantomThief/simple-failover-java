56d59557ae32c23bbac1939f828743c9d5ea1a80
w.vela <boywang004@hotmail.com>
2016-01-05 16:11:44 +0800

    remove proxy.

--- src/main/java/com/github/phantomthief/failover/util/FailoverUtils.java
+++ src/main/java/com/github/phantomthief/failover/util/FailoverUtils.java
@@ -3,18 +3,13 @@
  */
 package com.github.phantomthief.failover.util;
 
-import static java.util.function.Function.identity;
-
-import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 import com.github.phantomthief.failover.Failover;
 import com.github.phantomthief.failover.exception.NoAvailableResourceException;
 import com.github.phantomthief.util.ThrowableConsumer;
 import com.github.phantomthief.util.ThrowableFunction;
 import com.google.common.base.Throwables;
-import com.google.common.reflect.Reflection;
 
 /**
  * @author w.vela
@@ -64,34 +59,5 @@ public class FailoverUtils {
         } else {
             throw new NoAvailableResourceException();
         }
-    }
-
-    public static <T> T proxy(Class<T> iface, Failover<T> failover) {
-        return proxy(iface, failover, null);
-    }
-
-    public static <T> T proxy(Class<T> iface, Failover<T> failover,
-            Predicate<Throwable> failChecker) {
-        return proxy(iface, () -> failover, failChecker);
-    }
-
-    public static <T> T proxy(Class<T> iface, Supplier<Failover<T>> failover) {
-        return proxy(iface, failover, null);
-    }
-
-    public static <T> T proxy(Class<T> iface, Supplier<Failover<T>> failover,
-            Predicate<Throwable> failChecker) {
-        return transformProxy(iface, failover, identity(), failChecker);
-    }
-
-    public static <T, E> T transformProxy(Class<T> iface, Supplier<Failover<E>> failover,
-            Function<E, T> tranformFunction) {
-        return transformProxy(iface, failover, tranformFunction, null);
-    }
-
-    public static <T, E> T transformProxy(Class<T> iface, Supplier<Failover<E>> failover,
-            Function<E, T> tranformFunction, Predicate<Throwable> failChecker) {
-        return Reflection.newProxy(iface, (proxy, method, args) -> run(failover.get(),
-                res -> method.invoke(tranformFunction.apply(res), args), failChecker));
     }
 }

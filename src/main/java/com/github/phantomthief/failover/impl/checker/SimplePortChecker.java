cc5a6765212e01aee3301ce3e9def04d389725d7
w.vela <vela@kuaishou.com>
2017-06-14 13:22:56 +0800

    remove unnecessary log.

--- src/main/java/com/github/phantomthief/failover/impl/checker/SimplePortChecker.java
+++ src/main/java/com/github/phantomthief/failover/impl/checker/SimplePortChecker.java
@@ -3,11 +3,8 @@
  */
 package com.github.phantomthief.failover.impl.checker;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
 
 import com.google.common.net.HostAndPort;
 
@@ -18,42 +15,19 @@ public class SimplePortChecker {
 
     private static final int DEFAULT_CONNECTION_TIMEOUT = 5000;
 
-    private static org.slf4j.Logger logger = getLogger(SimplePortChecker.class);
-
     public static boolean check(String host, int port) {
-        return check(null, host, port);
-    }
-
-    @Deprecated
-    public static boolean check(String friendlyName, String host, int port) {
-        return check(friendlyName, host, port, DEFAULT_CONNECTION_TIMEOUT);
+        return check(host, port, DEFAULT_CONNECTION_TIMEOUT);
     }
 
     public static boolean check(HostAndPort hostAndPort) {
-        return check(null, hostAndPort);
-    }
-
-    @Deprecated
-    public static boolean check(String friendlyName, HostAndPort hostAndPort) {
-        return check(friendlyName, hostAndPort.getHost(), hostAndPort.getPort(),
-                DEFAULT_CONNECTION_TIMEOUT);
+        return check(hostAndPort.getHost(), hostAndPort.getPort());
     }
 
     public static boolean check(String host, int port, int connectionTimeoutInMs) {
-        return check(null, host, port, connectionTimeoutInMs);
-    }
-
-    @Deprecated
-    public static boolean check(String friendlyName, String host, int port,
-            int connectionTimeoutInMs) {
-        String logName = friendlyName == null ? "" : "[" + friendlyName + "]";
         try (Socket socket = new Socket()) {
-            SocketAddress sockaddr = new InetSocketAddress(host, port);
-            socket.connect(sockaddr, connectionTimeoutInMs);
-            logger.info("{}[{}:{}] is reachable.", logName, host, port);
+            socket.connect(new InetSocketAddress(host, port), connectionTimeoutInMs);
             return true;
         } catch (Throwable e) {
-            logger.warn("{}[{}:{}] is NOT reachable.", logName, host, port);
             return false;
         }
     }

956d9a3ce45ce7880625594a668d40e725561cde
w.vela <wangtianzhou@kuaishou.com>
2018-01-25 23:15:15 +0800

    Revert "[code healthy] remove deprecated RecoverableFailover."
    This reverts commit 459d3a2bcb2c7686bd77e163cf2867b6002ce5f6.

---
+++ src/main/java/com/github/phantomthief/failover/impl/GenericRecoverableCheckFailoverBuilder.java
@@ -0,0 +1,56 @@
+package com.github.phantomthief.failover.impl;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import javax.annotation.CheckReturnValue;
+
+/**
+ * @author w.vela
+ */
+@Deprecated
+public class GenericRecoverableCheckFailoverBuilder<E> {
+
+    private final RecoverableCheckFailoverBuilder<Object> builder;
+
+    GenericRecoverableCheckFailoverBuilder(RecoverableCheckFailoverBuilder<Object> builder) {
+        this.builder = builder;
+    }
+
+    @CheckReturnValue
+    public RecoverableCheckFailoverBuilder<Object> setFailCount(int failCount) {
+        return builder.setFailCount(failCount);
+    }
+
+    @CheckReturnValue
+    public GenericRecoverableCheckFailoverBuilder<E> setChecker(Predicate<? super E> checker) {
+        builder.setChecker(checker);
+        return this;
+    }
+
+    @CheckReturnValue
+    public GenericRecoverableCheckFailoverBuilder<E>
+            setRecoveryCheckDuration(long recoveryCheckDuration, TimeUnit unit) {
+        builder.setRecoveryCheckDuration(recoveryCheckDuration, unit);
+        return this;
+    }
+
+    @CheckReturnValue
+    public GenericRecoverableCheckFailoverBuilder<E> setFailDuration(long failDuration,
+            TimeUnit unit) {
+        builder.setFailDuration(failDuration, unit);
+        return this;
+    }
+
+    @CheckReturnValue
+    public GenericRecoverableCheckFailoverBuilder<E>
+            setReturnOriginalWhileAllFailed(boolean returnOriginalWhileAllFailed) {
+        builder.setReturnOriginalWhileAllFailed(returnOriginalWhileAllFailed);
+        return this;
+    }
+
+    public RecoverableCheckFailover<E> build(List<? extends E> original) {
+        return builder.build(original);
+    }
+}
---
+++ src/main/java/com/github/phantomthief/failover/impl/RecoverableCheckFailoverBuilder.java
@@ -0,0 +1,95 @@
+package com.github.phantomthief.failover.impl;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import javax.annotation.CheckReturnValue;
+
+import org.slf4j.Logger;
+
+@Deprecated
+public final class RecoverableCheckFailoverBuilder<T> {
+
+    private static final int DEFAULT_FAIL_COUNT = 10;
+    private static final long DEFAULT_FAIL_DURATION = MINUTES.toMillis(1);
+    private static final long DEFAULT_RECOVERY_CHECK_DURATION = SECONDS.toMillis(5);
+    private static final Logger logger = getLogger(RecoverableCheckFailover.class);
+    private int failCount;
+    private long failDuration;
+    private long recoveryCheckDuration;
+    private boolean returnOriginalWhileAllFailed;
+    private Predicate<T> checker;
+
+    @CheckReturnValue
+    public RecoverableCheckFailoverBuilder<T> setFailCount(int failCount) {
+        this.failCount = failCount;
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    @CheckReturnValue
+    public <E> RecoverableCheckFailoverBuilder<E> setChecker(Predicate<? super E> checker) {
+        RecoverableCheckFailoverBuilder<E> thisBuilder = (RecoverableCheckFailoverBuilder<E>) this;
+        thisBuilder.checker = thisBuilder.catching((Predicate<E>) checker);
+        return thisBuilder;
+    }
+
+    @CheckReturnValue
+    public RecoverableCheckFailoverBuilder<T> setRecoveryCheckDuration(long recoveryCheckDuration,
+            TimeUnit unit) {
+        this.recoveryCheckDuration = unit.toMillis(recoveryCheckDuration);
+        return this;
+    }
+
+    @CheckReturnValue
+    public RecoverableCheckFailoverBuilder<T> setFailDuration(long failDuration, TimeUnit unit) {
+        this.failDuration = unit.toMillis(failDuration);
+        return this;
+    }
+
+    @CheckReturnValue
+    public RecoverableCheckFailoverBuilder<T>
+            setReturnOriginalWhileAllFailed(boolean returnOriginalWhileAllFailed) {
+        this.returnOriginalWhileAllFailed = returnOriginalWhileAllFailed;
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <E> RecoverableCheckFailover<E> build(List<? extends E> original) {
+        RecoverableCheckFailoverBuilder<E> thisBuilder = (RecoverableCheckFailoverBuilder<E>) this;
+        thisBuilder.ensure();
+        return new RecoverableCheckFailover<>((List<E>) original, thisBuilder.checker, failCount,
+                failDuration, recoveryCheckDuration, returnOriginalWhileAllFailed);
+    }
+
+    private void ensure() {
+        checkNotNull(checker);
+
+        if (failCount <= 0) {
+            failCount = DEFAULT_FAIL_COUNT;
+        }
+        if (failDuration <= 0) {
+            failDuration = DEFAULT_FAIL_DURATION;
+        }
+        if (recoveryCheckDuration <= 0) {
+            recoveryCheckDuration = DEFAULT_RECOVERY_CHECK_DURATION;
+        }
+    }
+
+    private Predicate<T> catching(Predicate<T> predicate) {
+        return t -> {
+            try {
+                return predicate.test(t);
+            } catch (Throwable e) {
+                logger.error("Ops. fail to test:{}", t, e);
+                return false;
+            }
+        };
+    }
+}

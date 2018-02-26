c80780014dbf802676bc38caa70940ac5425a332
w.vela <wangtianzhou@kuaishou.com>
2018-02-26 16:32:24 +0800

    [performance] using LCG for random list.

--- src/main/java/com/github/phantomthief/failover/util/RandomListUtils.java
+++ src/main/java/com/github/phantomthief/failover/util/RandomListUtils.java
@@ -7,6 +7,7 @@ import static java.util.Collections.shuffle;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.RandomAccess;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.annotation.Nullable;
@@ -15,6 +16,8 @@ import javax.annotation.Nullable;
  * @author w.vela
  */
 public class RandomListUtils {
+
+    private static final int LCG_THRESHOLD = 3;
 
     private RandomListUtils() {
         throw new UnsupportedOperationException();
@@ -32,8 +35,27 @@ public class RandomListUtils {
         if (source == null || source.isEmpty()) {
             return emptyList();
         }
+        if (source instanceof List && source instanceof RandomAccess
+                && size < source.size() / LCG_THRESHOLD) {
+            return getRandomUsingLcg(source, size);
+        } else {
+            return getRandomUsingShuffle(source, size);
+        }
+    }
+
+    static <T> List<T> getRandomUsingShuffle(Collection<T> source, int size) {
         List<T> newList = new ArrayList<>(source);
         shuffle(newList, ThreadLocalRandom.current());
         return newList.subList(0, min(newList.size(), size));
+    }
+
+    static <T> List<T> getRandomUsingLcg(Collection<T> source, int size) {
+        int targetSize = min(source.size(), size);
+        List<T> newList = new ArrayList<>(targetSize);
+        RandomIterator<T> iterator = new RandomIterator<>((List<T>) source);
+        for (int i = 0; i < targetSize; i++) {
+            newList.add(iterator.next());
+        }
+        return newList;
     }
 }
---
+++ src/test/java/com/github/phantomthief/failover/util/RandomListUtilsTest.java
@@ -0,0 +1,72 @@
+package com.github.phantomthief.failover.util;
+
+import static com.github.phantomthief.failover.util.RandomListUtils.getRandomUsingLcg;
+import static com.github.phantomthief.failover.util.RandomListUtils.getRandomUsingShuffle;
+import static java.lang.System.nanoTime;
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author w.vela
+ * Created on 2018-02-26.
+ */
+class RandomListUtilsTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(RandomListUtilsTest.class);
+
+    @Test
+    void getRandom() {
+        long lcgBetter = 0, shuffleBetter = 0;
+        for (int i = 0; i < 1000; i++) {
+            int size = ThreadLocalRandom.current().nextInt(1, 10000);
+            int retrieved = ThreadLocalRandom.current().nextInt(1, size / 3);
+            List<Integer> list = IntStream.range(0, size).boxed().collect(toList());
+
+            long s = nanoTime();
+            List<Integer> result = getRandomUsingLcg(list, retrieved);
+            long costLcg = nanoTime() - s;
+
+            long s2 = nanoTime();
+            getRandomUsingShuffle(list, retrieved);
+            long costShuffle = nanoTime() - s2;
+
+            assertEquals(retrieved, result.stream().distinct().count());
+            if (costLcg > costShuffle) {
+                shuffleBetter += costLcg - costShuffle;
+            } else {
+                lcgBetter += costShuffle - costLcg;
+            }
+        }
+        logger.info("lcg better:{}, shuffle better:{}", lcgBetter, shuffleBetter);
+        lcgBetter = 0;
+        shuffleBetter = 0;
+        for (int i = 0; i < 1000; i++) {
+            int size = ThreadLocalRandom.current().nextInt(1, 10000);
+            List<Integer> list = IntStream.range(0, size).boxed().collect(toList());
+
+            long s = nanoTime();
+            List<Integer> result = getRandomUsingLcg(list, size);
+            long costLcg = nanoTime() - s;
+
+            long s2 = nanoTime();
+            getRandomUsingShuffle(list, size);
+            long costShuffle = nanoTime() - s2;
+
+            assertEquals(size, result.stream().distinct().count());
+            if (costLcg > costShuffle) {
+                shuffleBetter += costLcg - costShuffle;
+            } else {
+                lcgBetter += costShuffle - costLcg;
+            }
+        }
+        logger.info("lcg better:{}, shuffle better:{}", lcgBetter, shuffleBetter);
+    }
+}
